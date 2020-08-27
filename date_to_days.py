790ee0a67aa3864e52bdc32db89fd9d41431de4f
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-27 21:43:23 +1000

    Add days_to_date.py Add main() function to date_to_days.py Add function docstring for is_leap_year() in date_to_days.py Rename date_to_days.py to days_between_dates.py

--- date_to_days.py
+++ date_to_days.py
@@ -6,6 +6,12 @@ MONTHS_DICT = {1: 31, 2: 28, 3: 31,
                10: 31, 11: 30, 12: 31}
 
 
+def main():
+    """Take 2 dates and calculate the number of days between them."""
+    days_between_dates = convert_date_to_days('27/08/2020') - convert_date_to_days('27/05/2001')
+    print(days_between_dates)
+
+
 def convert_date_to_days(date: str) -> int:
     """Convert date to total days since 0 A.D."""
     total_days = 1  # Starting from Jan 1st, 0 A.D
@@ -13,21 +19,22 @@ def convert_date_to_days(date: str) -> int:
     year = date_list[2]
     month = date_list[1]
     day = date_list[0]
-    for x in range(1, year):
-        if is_leap_year(x):
+    for year_to_add in range(1, year):
+        if is_leap_year(year_to_add):
             total_days += 366
         else:
             total_days += 365
-    for x in range(1, month):
-        if x == 2 and is_leap_year(year):
+    for month_to_add in range(1, month):
+        if month_to_add == 2 and is_leap_year(year):
             total_days += 29
         else:
-            total_days += MONTHS_DICT[x]
+            total_days += MONTHS_DICT[month_to_add]
     total_days += day
     return total_days
 
 
 def is_leap_year(year):
+    """Determine if a given year is a leap year."""
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
@@ -41,5 +48,4 @@ def is_leap_year(year):
     return is_leap
 
 
-days_lived = convert_date_to_days('27/08/2020') - convert_date_to_days('27/05/2001')
-print(days_lived)
+main()
