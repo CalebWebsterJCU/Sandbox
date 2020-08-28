1ec1dc1587f16e1c72f0a4ccc8053b60a3cff5fe
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-28 12:42:42 +1000

    Complete days_between_dates.py Delete days_to_date.py

--- days_between_dates.py
+++ days_between_dates.py
@@ -1,5 +1,8 @@
 """This program takes a date and calculates how many days since Jan 1, 2000"""
 
+from datetime import datetime as d
+CURRENT_DATE = d.now()
+CURRENT_DATE_STRING = f"{CURRENT_DATE.day:02}/{CURRENT_DATE.month:02}/{CURRENT_DATE.year}"
 MONTHS_DICT = {1: 31, 2: 28, 3: 31,
                4: 30, 5: 31, 6: 30,
                7: 31, 8: 31, 9: 30,
@@ -8,17 +11,17 @@ MONTHS_DICT = {1: 31, 2: 28, 3: 31,
 
 def main():
     """Take 2 dates and calculate the number of days between them."""
-    days_between_dates = convert_date_to_days('27/08/2020') - convert_date_to_days('27/05/2001')
-    print(days_between_dates)
+    birth_date_string = get_valid_date_string("Birth Date: ")
+    total_days_lived = convert_date_to_days(CURRENT_DATE_STRING) - convert_date_to_days(birth_date_string)
+    years, months, days = calc_exact_age(total_days_lived)
+    print(f"Today is your {total_days_lived}{find_ordinal(total_days_lived)} day on earth.")
+    print(f"You are {years} years, {months} months, and {days} days old.")
 
 
-def convert_date_to_days(date: str) -> int:
+def convert_date_to_days(date_string: str) -> int:
     """Convert date to total days since 0 A.D."""
     total_days = 1  # Starting from Jan 1st, 0 A.D
-    date_list = [int(part) for part in date.split('/')]
-    year = date_list[2]
-    month = date_list[1]
-    day = date_list[0]
+    day, month, year = convert_date_string(date_string)
     for year_to_add in range(1, year):
         if is_leap_year(year_to_add):
             total_days += 366
@@ -31,6 +34,32 @@ def convert_date_to_days(date: str) -> int:
             total_days += MONTHS_DICT[month_to_add]
     total_days += day
     return total_days
+
+
+def get_valid_date_string(prompt: str) -> str:
+    """Get a valid date string input eg. '01/01/2000'"""
+    date_string = ""
+    is_valid = False
+    while not is_valid:
+        try:
+            date_string = input(prompt)
+            day, month, year = convert_date_string(date_string)
+            if year not in range(1, CURRENT_DATE.year + 1) or month not in range(1, 13) or day not in range(1, 32):
+                print("Invalid date")
+            else:
+                is_valid = True
+        except ValueError:
+            print("Invalid date")
+        except IndexError:
+            print("Invalid date")
+    return date_string
+
+
+def convert_date_string(date_string: str) -> tuple:
+    """Convert date string to day, month and year."""
+    date_list = [int(part) for part in date_string.split('/')]
+    day, month, year = date_list[0], date_list[1], date_list[2]
+    return day, month, year
 
 
 def is_leap_year(year):
@@ -46,6 +75,32 @@ def is_leap_year(year):
     else:
         is_leap = False
     return is_leap
+
+
+def calc_exact_age(total_days: int):
+    """Calculate exact age, given total days lived."""
+    years = int(total_days // 365.2425)
+    total_days %= 365.2425
+    months = int(total_days // 30.4167)
+    total_days %= 30.4167
+    days = int(total_days)
+    return years, months, days
+
+
+def find_ordinal(number):
+    """Given a number, find the ordinal."""
+    last_digit = number % 10
+
+    if last_digit == 1:
+        ordinal = "st"
+    elif last_digit == 2:
+        ordinal = "nd"
+    elif last_digit == 3:
+        ordinal = "rd"
+    else:
+        ordinal = "th"
+
+    return ordinal
 
 
 main()
--- days_to_date.py
+++
@@ -1,3 +0,0 @@
-"""This program takes a number of days and calculates the exact number of days, months, and years."""
-
-
