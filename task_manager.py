c005858b88b49a95672f7de9709823b05e0b924d
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-30 15:06:19 +1000

    Add task_manager.py Add tasks.csv

---
+++ task_manager.py
@@ -0,0 +1,9 @@
+"""
+Task Manager
+by Caleb Webster
+30/08/2020
+This program functions as a task list. It allows the user to add, complete and view tasks to improve their workflow.
+"""
+
+from operator import itemgetter
+FILE_NAME = "tasks.csv"
