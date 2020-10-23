a8e60923013c4bf18dd701ca60f51cea18f452ab
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-10-23 18:27:00 +1000

    Add meeting_times.py Add test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+"""
+Doctest test
+"""
+
+
+def mul(a, b):
+    """
+    >>> mul(1, 2)
+    2
+    >>> mul(3, 'b')
+    'bbb'
+    """
+    return a * b
+
+
+def add(a, b):
+    """
+    >>> add(1, 2)
+    3
+    >>> add('c', 'd')
+    'cd'
+    """
+    return a + b
