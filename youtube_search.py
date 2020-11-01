f46e18b6ae8051f02fd4a25becc0e427e3344113
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-11-01 22:05:36 +1000

    Complete script for building youtube search links and opening youtube

--- youtube_search.py
+++ youtube_search.py
@@ -1,3 +1,14 @@
 #! python3.7
 
-print("Hello World")
+import sys
+import webbrowser
+
+arguments = sys.argv
+
+if len(arguments) > 1:
+    search = "+".join(arguments[1:])
+    url = f"https://youtube.com/results?search_query={search}"
+else:
+    url = "https://youtube.com"
+
+webbrowser.open(url)
