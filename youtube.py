b2ee2e2b5dfa6b19af0d05fe0aa4b3b8c837aec0
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-11-10 14:01:48 +1000

    Extract program to main, add docstrings

--- youtube.py
+++ youtube.py
@@ -1,13 +1,24 @@
-#! python3.7
+#! python3
+"""
+Youtube
+Caleb Webster
+2020
+This program opens www.youtube.com and searches for a phrase if one is passed to the run command.
+"""
 
 import sys
 import webbrowser
 
-arguments = sys.argv
-url = "https://youtube.com"
 
-if len(arguments) > 1:
-    search = "+".join(arguments[1:])
-    url += f"/results?search_query={search}"
+def main():
+    """Open www.youtube.com, passing in any run command arguments as search terms."""
+    arguments = sys.argv
+    url = "https://youtube.com"
+    if len(arguments) > 1:
+        search = "+".join(arguments[1:])
+        url += f"/results?search_query={search}"
+    webbrowser.open(url)
 
-webbrowser.open(url)
+
+if __name__ == '__main__':
+    main()
