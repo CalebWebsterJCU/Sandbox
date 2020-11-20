b9748a3d61ade36ccca446c86aeb47822530d7a2
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-11-20 19:26:24 +1000

    Add game.py

---
+++ game.py
@@ -0,0 +1,12 @@
+"""
+Game
+2020
+This class represents a video game with a shortcut path, image, and last played date.
+"""
+
+
+class Game:
+    """
+    Represent a video game object.
+    attributes: self.path, self.image,
+    """
